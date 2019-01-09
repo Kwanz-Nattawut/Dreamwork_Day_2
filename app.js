0dd8b0b3a23581cb52df7a9daf33376b8f9dc616
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-09 13:59:14 +0700

    three commit

--- app.js
+++ app.js
@@ -2,11 +2,33 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('./mongoose');
+var Tempature = require('./models/temperature.model');
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send("Hello World");
-    res.send(mongoose.connection.readyState);
+});
+
+app.get('/showData', (req, res) => {
+    Tempature.find({}).exec((err,Temp) => {
+         res.json(Temp);
+    });
+});
+
+app.post('/addData', (req, res) => {
+    var NewTeam = new Temperature(req.body);
+
+    NewTeam.save((err,t) => {
+        if(err){
+            res.send(err);
+        }
+        else
+        {
+             res.json(t);
+        }
+    });
+
 });
 
 
---
+++ models/temperature.model.js
@@ -0,0 +1,10 @@
+var mongoose = require('../mongoose');
+
+var Schema = mongoose.Schema;
+
+var TemperatureSchema = new Schema({
+    teamID : String,
+    temp : Number
+});
+
+module.exports = mongoose.model('Temperature',TemperatureSchema);
