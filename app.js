6897e6edd7f86f9a4897a332728e9caafe1edc31
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-10 10:29:51 +0700

    six commit

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post('/receiveData', (req, res) => {
     var tempReceiveData = new Temperature();
     
-    //var payload = req.body.DevEUI_up.payload_hex;
-    console.log(req.body);
+    var payload = req.body.DevEUI_uplink.payload_hex;
+    if(payload){
+        console.log(payload);
+    }
+    else
+    {
+        console.log(req.body);
+    }
+    
     /*var temp = (parseInt(payload.slice(4,8),16) * 0.1).toFixed(2);
     var teamID = (parseInt(payload.slice(12,16),16) *0.01);
 
